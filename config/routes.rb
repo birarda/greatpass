07f38fbb3fc01a4c49dfb3a0a40a106a9c52a86d
Stephen Birarda <commit@birarda.com>
2016-09-05 21:43:08 -0700

    add sign up and login for logged out users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :user do
