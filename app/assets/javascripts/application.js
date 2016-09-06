84c4e0e0009e2d73297c8db6f5c4f7a949cdcca7
Stephen Birarda <commit@birarda.com>
2016-09-05 23:32:24 -0700

    use js to select nav active, start search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,14 @@
 //= require turbolinks
 //= require selectize
 //= require_tree .
+
+$(document).on('turbolinks:load', function () {
+    var url = window.location;
+    // Will only work if string in href matches with location
+    $('ul.nav a[href="' + url + '"]').parent().addClass('active');
+
+    // Will also work for relative and absolute hrefs
+    $('ul.nav a').filter(function () {
+        return this.href == url;
+    }).parent().addClass('active').parent().parent().addClass('active');
+});
