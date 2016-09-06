f6549c000ae1cb971f9f1180ffaa05a8b9c313bd
Stephen Birarda <commit@birarda.com>
2016-09-06 11:55:43 -0700

    fix caching of already selectized selects

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,9 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).on('turbolinks:load', function() {
-  $('#search__item_id').selectize();
-  $('#search__certification').selectize();
-  $('#search__paint_color').selectize();
-  $('#search__platform').selectize();
-  $('#search__kind').selectize();
+  var $selectized_selects = [$('#search__item_id'), $('#search__certification'), $('#search__paint_color'), $('#search__platform'), $('#search__kind')]
+  $.each($selectized_selects, function(index, $select){
+    if (!$select.hasClass('selectized')) {
+      $select.selectize();
+    }
+  })
 });
