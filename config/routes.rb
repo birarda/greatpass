4874c5837e3a7ed949e97d9fe9c71120e2875f14
Stephen Birarda <commit@birarda.com>
2016-09-05 21:52:56 -0700

    navbar updates, add simple flash handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'items#index'
+
+  resources :items, only: [:index]
   namespace :user do
     resources :items, only: [:index, :create, :destroy]
   end
