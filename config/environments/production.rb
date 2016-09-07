7e34489164fd2350cfb0529ef334079ed8602b1a
Stephen Birarda <commit@birarda.com>
2016-09-06 22:35:52 -0700

    fix typo in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.devliery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     from: 'contact@greatpass.io',
     address: 'smtp.sparkpostmail.com',
