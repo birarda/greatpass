7fc4159729477a3f50008a5647419561b513dd21
Stephen Birarda <commit@birarda.com>
2016-09-08 17:42:11 -0700

    add rails_admin, scope to admin user

--- Gemfile
+++ Gemfile
@@ -55,6 +55,9 @@ gem 'bugsnag'
 
 gem 'validate_url'
 
+gem 'remotipart', github: 'mshibuya/remotipart'
+gem 'rails_admin', '>= 1.0.0.rc'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
