5685456651cb4b14ed72ba227c6f048d623c584f
Stephen Birarda <commit@birarda.com>
2016-09-11 10:25:28 -0700

    force TCP connection to postgresql in db_backup

--- lib/cron/db_backup.sh
+++ lib/cron/db_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 NOW=$(date +"%Y%m%d_%H%M%S")
-cd /home/birarda/pg-backups;/usr/bin/pg_dump -U greatpass greatpass_production | gzip > gp-db.$NOW.sql.gz
+cd /home/birarda/pg-backups;/usr/bin/pg_dump -U greatpass -h localhost greatpass_production | gzip > gp-db.$NOW.sql.gz
 
 /home/birarda/.local/bin/aws s3 mv gp-db.$NOW.sql.gz s3://gp-db-prod/db-backup/$1/
