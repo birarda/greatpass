0bf826c522e41392339647762e5dc6164f3394ed
Stephen Birarda <commit@birarda.com>
2016-09-10 19:44:55 -0700

    put credential init on same line because why not

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,4 @@
 Aws.config.update({
   region: 'us-west-1',
-  credentials: Aws::Credentials.new(
-    ENV['GP_AWS_ACCESS_KEY_ID'],
-    ENV['GP_AWS_SECRET_KEY_ID']
-  )
+  credentials: Aws::Credentials.new(ENV['GP_AWS_ACCESS_KEY_ID'], ENV['GP_AWS_SECRET_KEY_ID'])
 })
