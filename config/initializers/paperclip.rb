681882645f99778b36bc5e0326313b5892e852b9
Stephen Birarda <commit@birarda.com>
2016-09-10 20:34:29 -0700

    set s3_host_name in credentials

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,10 +2,10 @@ Paperclip::Attachment.default_options[:storage] = :s3
 Paperclip::Attachment.default_options[:url] = ':s3_alias_url'
 Paperclip::Attachment.default_options[:s3_host_alias] = Settings.s3.host_alias
 Paperclip::Attachment.default_options[:s3_region] = 'us-west-1'
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-1.amazonaws.com'
 
 Paperclip::Attachment.default_options[:s3_credentials] = {
   bucket: Settings.s3.bucket_name,
   access_key_id: ENV['GP_AWS_ACCESS_KEY_ID'],
-  secret_access_key: ENV['GP_AWS_SECRET_KEY_ID']
+  secret_access_key: ENV['GP_AWS_SECRET_KEY_ID'],
+  s3_host_name: 's3-us-west-1.amazonaws.com'
 }
