66cebf028f1962ed719c845fff6e4f16c7e14498
Stephen Birarda <commit@birarda.com>
2016-09-11 10:16:38 -0700

    use user installation of aws-sdk cli

--- lib/cron/db_backup.sh
+++ lib/cron/db_backup.sh
@@ -3,4 +3,4 @@
 NOW=$(date +"%Y%m%d_%H%M%S")
 cd /home/birarda/pg-backups;/usr/bin/pg_dump -U greatpass greatpass_production | gzip > gp-db.$NOW.sql.gz
 
-/usr/local/bin/aws s3 mv gp-db.$NOW.sql.gz s3://gp-db-prod/db-backup/$1/
+/home/birarda/.local/bin/aws s3 mv gp-db.$NOW.sql.gz s3://gp-db-prod/db-backup/$1/
