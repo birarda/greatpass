348ff6031c3499b2290d74ac636f50c70b85745f
Stephen Birarda <commit@birarda.com>
2016-09-13 13:08:17 -0700

    add a matchmaking page

---
+++ app/assets/javascripts/matchmaker.js
@@ -0,0 +1,15 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+$(document).on('turbolinks:load', function() {
+  var $selectized_selects = [$('#match_have__item_id'), $('#match_have__certification'), $('#match_have__paint_color'), $('#match_want__item_id'), $('#match_have__platform'), $('#match_want__certification'), $('#match_want__paint_color'), $('#match_want__platform')]
+  $.each($selectized_selects, function(index, $select){
+    $select.selectize();
+  });
+});
+
+
+$(document).on('turbolinks:before-cache', function() {
+  $('.selectized').each(function() {
+    this.selectize.destroy();
+  });
+});
