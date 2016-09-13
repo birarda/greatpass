7ba46c8b8c9d878aee71ea8821619acdf9db4363
Stephen Birarda <commit@birarda.com>
2016-09-13 01:15:48 -0700

    remove console log of event target

--- app/assets/javascripts/user/messages.js
+++ app/assets/javascripts/user/messages.js
@@ -1,7 +1,6 @@
 $(document).on('turbolinks:load', function() {
 
   $('#user-messages tr.message-row').click(function(event){
-    console.log($(event.target));
     if (!$(event.target).is('a') && !$(event.target).is('span.glyphicon')) {
       window.location = $(this).data('conversation-link');
     }
