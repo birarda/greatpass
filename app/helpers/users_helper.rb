88cd4df79e5c2c3614f84a5fa28d3861f2bbc6ef
Stephen Birarda <commit@birarda.com>
2016-09-13 14:10:54 -0700

    fix some bad html safety

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,14 +10,14 @@ module UsersHelper
   end
 
   def platform_username_with_ion(platform_user)
-    "<span class='platform-username-with-ion'><i class='icon #{platform_ion_class(platform_user.platform)}'></i>&nbsp;#{platform_user.platform_username}</span>".html_safe
+    "<span class='platform-username-with-ion'><i class='icon #{platform_ion_class(platform_user.platform)}'></i>&nbsp;#{sanitize(platform_user.platform_username)}</span>".html_safe
   end
 
   def button_popover_for_user(user)
     popover_html = ""
 
     if user != current_user
-      popover_html += "<button type='button' class='btn btn-primary message-button' data-toggle='modal' data-target='#message-modal' data-receiver-id='#{user.id}'>Message<span class='hidden-username'>#{user.platform_username}</span></button>"
+      popover_html += "<button type='button' class='btn btn-primary message-button' data-toggle='modal' data-target='#message-modal' data-receiver-id='#{user.id}'>Message<span class='hidden-username'>#{sanitize(user.platform_username)}</span></button>"
     end
 
     popover_html += "<a id='popover-inventory-btn' class='btn btn-primary' href='#{platform_user_path(platform_string: user.platform.downcase, platform_username: user.platform_username)}'>Inventory</button>"
