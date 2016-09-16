fbb663b210ecce2fdb3ba878c685b813fec84869
Stephen Birarda <commit@birarda.com>
2016-09-15 21:20:01 -0700

    close after select and blur in main search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,7 +3,12 @@
 $(document).on('turbolinks:load', function() {
   var $selectized_selects = [$('#search__item_id'), $('#search__certification'), $('#search__paint_color'), $('#search__rare_class'), $('#search__platform'), $('#search__kind')]
   $.each($selectized_selects, function(index, $select){
-    $select.selectize();
+    $select.selectize({
+      closeAfterSelect: true,
+      onDropdownClose: function(dropdown) {
+        $(dropdown).prev().find('input').blur();
+      }
+    });
   });
 });
 
