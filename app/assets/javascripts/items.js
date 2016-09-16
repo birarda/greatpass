523358abc66b84a33fe3230cb982aaeb768ec4c6
Stephen Birarda <commit@birarda.com>
2016-09-15 21:12:47 -0700

    add simple panel toggling on type change

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,7 +1,29 @@
 $(document).on('turbolinks:load', function() {
   $('.unveil-img').unveil(100);
+
+  $('#search__kind').change(function(){
+    // the kind has just been changed
+    var newKinds = $(this).val();
+
+    if (newKinds) {
+      // first we filter the image panels to show only the selected kinds
+      $('.kind-panel').each(function(){
+
+        var thisKindID = $(this).data('kind-id');
+
+        if (newKinds.indexOf(thisKindID.toString()) == -1) {
+          $(this).hide();
+        } else {
+          $(this).show();
+        }
+      });
+    } else {
+      $('.kind-panel').show();
+    }
+  });
 });
 
 $(document).on('turbolinks:before-cache', function() {
   $(window).off('unveil');
+
 });
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,14 @@
 class ItemsController < ApplicationController
 
   def index
+    # setup the special options for certification/paint color
+    @certification_options = UserItem.certifications.sort.map { |k,v| [k.capitalize, v] }
+    @certification_options.insert(0, ['Any', -1], ['None', -2])
 
+    @paint_options = UserItem.paint_colors.sort.map { |k, v| [k.titleize, v] }
+    @paint_options.insert(0, ['Any', -1], ['None', -2])
+
+    @search_params = {}
   end
 
   def search
@@ -25,82 +32,80 @@ class ItemsController < ApplicationController
       query = UserItem.inventory
     end
 
-    # if all query params are empty, simply return all sorted by recency
-    if !@search_params.empty?
+    if @search_params.has_key?(:item_slug)
+      @hero_item = Item.find_by_url_slug(@search_params[:item_slug].downcase)
 
-      if @search_params.has_key?(:item_slug)
-        @hero_item = Item.find_by_url_slug(@search_params[:item_slug].downcase)
-
-        if !@hero_item
-          @hero_item ||= Item.find(@search_params[:item_slug].to_i)
-        end
-
-        @search_params[:item_id] = [@hero_item.id]
+      if !@hero_item
+        @hero_item ||= Item.find(@search_params[:item_slug].to_i)
       end
 
-      if @search_params.has_key?(:item_id)
-        query = query.where(item_id: @search_params[:item_id])
-      end
+      @search_params[:item_id] = [@hero_item.id]
+    end
 
-      # if we have a platform_string (a clean path to a platform user)
-      # then convert that to the platform array so it can be used below
+    if @search_params.has_key?(:item_id)
+      query = query.where(item_id: @search_params[:item_id])
+    end
+
+    # if we have a platform_string (a clean path to a platform user)
+    # then convert that to the platform array so it can be used below
+    if @search_params.has_key?(:platform_string)
+      @search_params[:platform] = [platform_from_url_string(@search_params[:platform_string])]
+    end
+
+    # if we have a current logged in user, default to searching on their platform
+    @search_params[:platform] = User.platforms[current_user.platform] if current_user && !@search_params.has_key?(:platform)
+
+    if @search_params.has_key?(:platform)
+      query = query.where(users: { platform: @search_params[:platform] })
+    end
+
+    if @search_params.has_key?(:platform_username)
       if @search_params.has_key?(:platform_string)
-        @search_params[:platform] = [platform_from_url_string(@search_params[:platform_string])]
-      end
+        @hero_user = User.where(
+          'platform_username ILIKE ? AND platform = ?',
+          @search_params[:platform_username],
+          platform_from_url_string(@search_params[:platform_string])
+        ).first
 
-      if @search_params.has_key?(:platform)
-        query = query.where(users: { platform: @search_params[:platform] })
-      end
-
-      if @search_params.has_key?(:platform_username)
-        if @search_params.has_key?(:platform_string)
-          @hero_user = User.where(
-            'platform_username ILIKE ? AND platform = ?',
-            @search_params[:platform_username],
-            platform_from_url_string(@search_params[:platform_string])
-          ).first
-
-          if @hero_user
-            query = query.references(:users).where(users: { id: @hero_user.id })
-          else
-            @result_items = UserItem.none.page(params[:page])
-            return
-          end
+        if @hero_user
+          query = query.references(:users).where(users: { id: @hero_user.id })
         else
-          query = query.references(:users).where('platform_username ILIKE ?', @search_params[:platform_username])
+          @result_items = UserItem.none.page(params[:page])
+          return
         end
+      else
+        query = query.references(:users).where('platform_username ILIKE ?', @search_params[:platform_username])
       end
+    end
 
-      [:certification, :paint_color].each do |filter|
-        if @search_params.has_key?(filter)
-          # pull out the Any/None options
-          query_any = @search_params[filter].include?('-1')
-          query_none = @search_params[filter].include?('-2')
+    [:certification, :paint_color].each do |filter|
+      if @search_params.has_key?(filter)
+        # pull out the Any/None options
+        query_any = @search_params[filter].include?('-1')
+        query_none = @search_params[filter].include?('-2')
 
-          if !query_any || !query_none
-            if !@search_params[filter].empty?
-              if query_any == query_none
-                query = query.where("#{filter.to_s} IN (?)", @search_params[filter])
-              else
-                query = query.where("#{filter.to_s} IN (?) OR #{filter.to_s} is #{query_any ? 'NOT NULL' : 'NULL'}", @search_params[filter])
-              end
-            elsif query_any
-              query = query.where("#{filter.to_s} is NOT NULL")
-            elsif query_none
-              query = query.where("#{filter.to_s} is NULL")
+        if !query_any || !query_none
+          if !@search_params[filter].empty?
+            if query_any == query_none
+              query = query.where("#{filter.to_s} IN (?)", @search_params[filter])
+            else
+              query = query.where("#{filter.to_s} IN (?) OR #{filter.to_s} is #{query_any ? 'NOT NULL' : 'NULL'}", @search_params[filter])
             end
+          elsif query_any
+            query = query.where("#{filter.to_s} is NOT NULL")
+          elsif query_none
+            query = query.where("#{filter.to_s} is NULL")
           end
         end
       end
+    end
 
-      if @search_params.has_key?(:kind)
-        query = query.where(items: { kind: @search_params[:kind] })
-      end
+    if @search_params.has_key?(:kind)
+      query = query.where(items: { kind: @search_params[:kind] })
+    end
 
-      if @search_params.has_key?(:rare_class)
-        query = query.where(items: { rare_class: @search_params[:rare_class] })
-      end
-
+    if @search_params.has_key?(:rare_class)
+      query = query.where(items: { rare_class: @search_params[:rare_class] })
     end
 
     @result_items = query.includes(:item, :user).order(created_at: :desc).page(params[:page])
