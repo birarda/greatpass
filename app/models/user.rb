2342aa7386ee67a05b319fc1acaf2679ad8be4b9
Stephen Birarda <commit@birarda.com>
2016-09-20 23:18:05 -0700

    delay devise emails for user after_commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,6 +48,7 @@ class User < ApplicationRecord
 
   before_validation :smart_add_platform_url_protocol
 
+  after_commit :send_pending_notifications
   after_create :send_manual_optional_confirmation_instructions
 
   def inbox
@@ -77,4 +78,31 @@ class User < ApplicationRecord
     self.skip_reconfirmation!
     self.send_confirmation_instructions
   end
+
+  def send_devise_notification(notification, *args)
+    # If the record is new or changed then delay the
+    # delivery until the after_commit callback otherwise
+    # send now because after_commit will not be called.
+    if new_record? || changed?
+      pending_notifications << [notification, args]
+    else
+      message = devise_mailer.send(notification, self, *args).deliver_later
+    end
+  end
+
+  def send_pending_notifications
+    pending_notifications.each do |notification, args|
+      message = devise_mailer.send(notification, self, *args).deliver_later
+    end
+
+    # Empty the pending notifications array because the
+    # after_commit hook can be called multiple times which
+    # could cause multiple emails to be sent.
+    pending_notifications.clear
+  end
+
+  def pending_notifications
+    @pending_notifications ||= []
+  end
+
 end
